55ab0ffb616cb727f8009af491aa2e351c3243d1
romangutikov <romangutikov@gmail.com>
2020-10-04 03:52:47 +0300

    fix: array solution
    array solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-
-exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.min = (array) => {
+    if (array === undefined || array[0] === undefined) return 0;
+    return Math.min(...array);
+ }
+ 
+ exports.max = function max (array) {
+   if (array === undefined || array[0] === undefined) return 0;
+   return Math.max(...array);
+ }
+ 
+ exports.avg = (array) => {
+   if (array === undefined || array[0] === undefined) return 0;
+ 
+   let count = 0;
+   for (let i = 0; i < array.length; i++) {
+     count += array[i]
+   }
+   return count / array.length;
+ }
